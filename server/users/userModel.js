c2c6690c4b23a79631a050dcdf03f6098946fd03
David Hom <dlhom3@gmail.com>
2015-06-22 15:08:17 -0700

    (feat) added user schema close #6

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -1,15 +1,10 @@
 var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+  name: { type: String, required: true },
+  fb_id: { type: Number, required: true, unique: true },
+  email: { type: String }, 
+  username: { type: Number, required: true, unique: true }
 });
 
 module.exports = mongoose.model('User', UserSchema);
